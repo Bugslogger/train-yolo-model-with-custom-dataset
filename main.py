4e8ff3da4a50327e93a687e99a78fb7fc1619cf4
sarfaraj <sarfaraj.s@nissisoftwaresystems.com>
2024-11-04 11:12:09 +0530

    pull

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from convert_xml_to_yolo import convert_voc_to_yolo
 # Example usage
 xml_folder = 'test_dataset/annotations'  # Folder containing your .xml files
 output_folder = 'trained_file'  # Folder to save YOLO .txt files
-class_names = ['hard hat', 'helmet', 'hat','person']  # List of your class names
+class_names = ['hard hat', 'helmet', 'hat','person','dog']  # List of your class names
 images_path = 'test_dataset/images'
 
 # below function call converts xml file into .txt file which will be used to train modal
